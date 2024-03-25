a59889ba9024001970b25c1cacb730a8d05c9b96
Jack Davidson <133827574+jdavid287@users.noreply.github.com>
2024-03-25 20:42:56 +0000

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Jeff and MSU!
+          Learn React with Jeff and Jack and MSU!
         </a>
       </header>
     </div>
